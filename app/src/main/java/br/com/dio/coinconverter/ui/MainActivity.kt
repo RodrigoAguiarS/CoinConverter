0b34e08083563db1e3c4f934bf6648c02cd44f3d
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2021-10-08 11:02:17 -0300

    concluindo a View

--- app/src/main/java/br/com/dio/coinconverter/ui/MainActivity.kt
+++ app/src/main/java/br/com/dio/coinconverter/ui/MainActivity.kt
@@ -1,7 +1,13 @@
 package br.com.dio.coinconverter.ui
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import br.com.dio.coinconverter.App
+import br.com.dio.coinconverter.core.extensions.text
+import br.com.dio.coinconverter.data.model.Coin
 import br.com.dio.coinconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +17,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        bindAdapters()
+        bindListeners()
     }
+
+    private fun bindListeners() {
+        binding.txtValue.editText?.doAfterTextChanged {
+            binding.btnConverter.isEnabled = it != null && it.toString().isNotEmpty()
+        }
+        binding.btnConverter.setOnClickListener {
+            Log.e("TAG", "bindingListeners" + binding.txtValue.text)
+
+        }
+    }
+
+    private fun bindAdapters() {
+        val list = Coin.values()
+        val adapter = ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, list)
+
+        binding.txtAtCompleteFrom.setAdapter(adapter)
+        binding.txtAtCompleteTo.setAdapter(adapter)
+
+        binding.txtAtCompleteFrom.setText(Coin.BRL.name, false)
+        binding.txtAtCompleteTo.setText(Coin.USD.name, false)
+    }
+
 }
